df07d7e46dc4de081aa105133318d24f8d77bb6b
ben west <benwest0291@outlook.com>
2021-11-12 16:08:29 +0000

    completed first section 1 & 2

--- First Component/app.js
+++ First Component/app.js
@@ -2,7 +2,7 @@ class Hello extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello there!</h1>
+        <h1>Hello</h1>
         <h1>Hello there!</h1>
         <h1>Hello there!</h1>
       </div>
