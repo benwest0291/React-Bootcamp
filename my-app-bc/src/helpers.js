41f7c0f20a767fb39cca140757a1b0260550a074
ben west <benwest0291@outlook.com>
2021-11-16 16:46:41 +0000

    setting up modules

---
+++ my-app-bc/src/helpers.js
@@ -0,0 +1,13 @@
+function helpful() {
+  console.log("This function is trying to be helpfull");
+}
+
+function sort() {
+  console.log("All Sorted");
+}
+
+function singing() {
+  console.log("LA LA LA LA ");
+}
+
+export { helpful, sort, singing };
