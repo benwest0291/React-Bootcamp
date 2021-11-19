e6733be6dcc0d51f8dabbb2bb691ce62d79945a0
ben west <benwest0291@outlook.com>
2021-11-19 12:13:42 +0000

    added comments

--- my-app-bc/src/index.js
+++ my-app-bc/src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 
+// importing functions from helpers.js
+
 import { helpful, sort, singing } from "./helpers";
+
+// calling functions from helpers/js
 
 helpful();
 sort();
